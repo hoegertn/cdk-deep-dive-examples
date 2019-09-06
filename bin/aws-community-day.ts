bef7f8f30aca8c91eeae0db9fccb3b8e212997e8
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2019-09-06 15:23:09 +0200

    more snippets

--- bin/aws-community-day.ts
+++ bin/aws-community-day.ts
@@ -6,13 +6,23 @@ import { MigrationStack } from '../lib/migration-stack';
 import { ExistingStack } from '../lib/existing-stack';
 import { AspectsStack } from '../lib/aspects-stack';
 import { CustomResourceStack } from '../lib/custom-resource-stack';
+import { ContextStack } from '../lib/context';
+import { LambdaStack } from '../lib/lambda';
+import { S3DeployStack } from '../lib/s3deploy';
 
-const pgarbe  = { account: '424144556073', region: 'eu-west-1' };
+const pgarbe = { account: '424144556073', region: 'eu-west-1' };
+const hoegertn = { account: '659154734889', region: 'eu-central-1' };
+const current = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
 const app = new cdk.App();
 
 new AwsCommunityDayStack(app, 'AwsCommunityDayStack');
 new MigrationStack(app, 'MigrationStack');
-new ExistingStack(app, 'ExistingStack', { env: pgarbe });
-new AspectsStack(app, 'AspectsStack', { env: pgarbe });
+new AspectsStack(app, 'AspectsStack', { env: current });
 new CustomResourceStack(app, 'CustomResourceStack');
+new ExistingStack(app, 'ExistingStack', { env: current });
+
+new ContextStack(app, 'ContextStack', { env: current });
+
+new LambdaStack(app, 'LambdaStack', { env: current });
+new S3DeployStack(app, 'S3DeployStack', { env: current });
